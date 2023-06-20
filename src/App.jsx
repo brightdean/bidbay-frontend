df167339e757f425b7ad3e89e8dfd1354bb4a3b7
brightDeanz <konstantinos.vroustouris@gmail.com>
2023-06-20 12:50:40 +0300

    publice/protected routing done, auth done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { Route, Routes } from 'react-router'
 import './App.css'
+import Layout from './routes/Layout'
+import { dashboardRoute, homeRoute, loginRoute, registerRoute } from './routes'
+import WelcomePage from './pages/WelcomePage'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+import RequireAuth from './routes/RequireAuth'
+import ProtectedPage from './pages/ProtectedPage'
+import DashboardPage from './pages/DashboardPage'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        {/* Public Routes */}
+        <Route index path={homeRoute} element={<WelcomePage />} />
+        <Route path={loginRoute} element={<LoginPage />} />
+        <Route path={registerRoute} element={<RegisterPage />} />
 
-    </>
+
+        {/* ProtectedRoutes */}
+        <Route element={<RequireAuth />}>
+          <Route path={dashboardRoute} element={<DashboardPage />} />
+        </Route>
+
+      </Route>
+    </Routes>
   )
 }
 
